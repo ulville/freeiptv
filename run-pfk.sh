abcbdd36d147c3258d18fb968b169c8b0f832e16
ulville <50755946+ulville@users.noreply.github.com>
2023-01-29 04:49:26 +0300

    add git-push

--- run-pfk.sh
+++ run-pfk.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+echo "Fetching Upstream..."
 git fetch upstream &&
+echo "Merging Upstream..."
 git merge upstream/master --no-edit &&
+echo "Commiting merged changes..."
 git commit -m "Merge changes from upstream $(date -I)" &&
+echo "Parsing for Kodi..."
 /usr/bin/python3 parse-for-kodi.py &&
-git commit -m "parsed for kodi $(date -I)"
+echo "Commiting parsing results..."
+git commit -m "parsed for kodi $(date -I)" &&
+echo "Pushing to remote..."
+git push
