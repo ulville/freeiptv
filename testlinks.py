15dee8bed26a5930f9298a093c89758766beb1a1
ulville <50755946+ulville@users.noreply.github.com>
2023-12-21 02:57:14 +0300

    dump successful to file

--- testlinks.py
+++ testlinks.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from subprocess import run
+import os
 import re
 import m3u8
 from m3u8 import protocol
@@ -8,10 +8,10 @@ from m3u8.parser import save_segment_custom_value
 from urllib.error import HTTPError, URLError
 from http.client import InvalidURL
 
-def dumps_m3u(m3u : M3U8):
+def dumps_iptv(iptv : M3U8):
     output = ["#EXTM3U"]
     last_group = ""
-    for seg in m3u.segments:
+    for seg in iptv.segments:
         segdumps = []
         seg_props = seg.custom_parser_values["extinf_props"]
         if seg_props["group-title"] != last_group and last_group != "":
@@ -30,6 +30,20 @@ def dumps_m3u(m3u : M3U8):
             segdumps.append(seg.uri)
         output.append("".join(segdumps))
     return "\n".join(output)
+
+def dump_iptv(iptv, filename):
+    """
+    Saves the current m3u8 to ``filename``
+    """
+    create_sub_directories(filename)
+
+    with open(filename, "w") as fileobj:
+        fileobj.write(dumps_iptv(iptv))
+
+def create_sub_directories(filename):
+    basename = os.path.dirname(filename)
+    if basename:
+        os.makedirs(basename, exist_ok=True)
 
 
 def parse_iptv_attributes(line, lineno, data, state):
@@ -66,48 +80,75 @@ def parse_iptv_attributes(line, lineno, data, state):
         # Tell 'main parser' that it can go to next line, we've parsed current fully.
         return True
 
+def is_success(playlist):
+    try:
+        m3u8.load(playlist.absolute_uri, 10)
+        return True
+    except (HTTPError, URLError, TimeoutError, InvalidURL):
+        return False
+
+
+def get_failed_links(channel_list):
+    failed = []
+    for i, s in enumerate(channel_list.segments):
+        try:
+            m = m3u8.load(s.uri, 10)
+            if m.is_variant:
+                if not any([is_success(pl) for pl in m.playlists]):
+                    failed.append(s)
+        except (HTTPError, URLError, TimeoutError, InvalidURL) as e:
+            failed.append(s)
+        print(f"\033[2Ktested {i} / {len(channel_list.segments)}", end="\r")
+    return failed
+            
 
 if __name__ == "__main__":
     FILE = "tr.m3u"
 
     parsed = m3u8.load(FILE, custom_tags_parser=parse_iptv_attributes)
-    print(parsed.is_variant)
+    for failed_segment in get_failed_links(parsed):
+        parsed.segments.remove(failed_segment)
+    
+    dump_iptv(parsed, "success.m3u")
 
-    for s in parsed.segments:
-        segment_props = s.custom_parser_values["extinf_props"]
-        print("title:", s.title)
-        print("logo:", segment_props["tvg-logo"])
-        print("group:", segment_props["group-title"])
-        print("uri:", s.uri)
-        try:
-            m = m3u8.load(s.uri, 5)
-            print("is_vaiant:", m.is_variant)
-            if m.is_variant:
-                for playlist in m.playlists:
-                    print("    ***")
-                    print("    playlist.uri:", playlist.uri)
-                    print("    playlist.absolute_uri:", playlist.absolute_uri)
-                    print("    playlist.stream_info:", playlist.stream_info)
-                    print("        * PLAYLIST CONTENT:")
-                    try:
-                        pm = m3u8.load(playlist.absolute_uri)
-                        print("        is_variant:", pm.is_variant)
-                        assert not pm.is_variant
-                        for plts in pm.segments:
-                            print("        \033[36mts.absolute_uri:\033[0m", plts.absolute_uri)
-                            print("        ts.duration:", plts.duration)
-                    except (HTTPError, URLError, TimeoutError, InvalidURL) as e:
-                        print("    \033[31mFailed!:\033[0m", e)
-            else:
-                print("    ***")
-                print("    SEGMENTS:")
-                for ts in m.segments:
-                    print("        \033[33mts.absolute_uri:\033[0m", ts.absolute_uri)
-                    print("        ts.duration:", ts.duration)
-        except (HTTPError, URLError, TimeoutError, InvalidURL) as e:
-            print("\033[31mFailed!:\033[0m", e)
+    # print(parsed.is_variant)
 
-        print("-" * 70)
+    # for s in parsed.segments:
+    #     segment_props = s.custom_parser_values["extinf_props"]
+    #     print("title:", s.title)
+    #     print("logo:", segment_props["tvg-logo"])
+    #     print("group:", segment_props["group-title"])
+    #     print("uri:", s.uri)
+    #     try:
+    #         m = m3u8.load(s.uri, 5)
+    #         print("is_vaiant:", m.is_variant)
+    #         if m.is_variant:
+    #             for playlist in m.playlists:
+    #                 print("    ***")
+    #                 print("    playlist.uri:", playlist.uri)
+    #                 print("    playlist.absolute_uri:", playlist.absolute_uri)
+    #                 print("    playlist.stream_info:", playlist.stream_info)
+    #                 print("        * PLAYLIST CONTENT:")
+    #                 try:
+    #                     pm = m3u8.load(playlist.absolute_uri)
+    #                     print("        is_variant:", pm.is_variant)
+    #                     assert not pm.is_variant
+    #                     for plts in pm.segments:
+    #                         print("        \033[36mts.absolute_uri:\033[0m", plts.absolute_uri)
+    #                         print("        ts.duration:", plts.duration)
+    #                 except (HTTPError, URLError, TimeoutError, InvalidURL) as e:
+    #                     print("    \033[31mFailed!:\033[0m", e)
+    #         else:
+    #             print("    ***")
+    #             print("    SEGMENTS:")
+    #             for ts in m.segments:
+    #                 print("        \033[33mts.absolute_uri:\033[0m", ts.absolute_uri)
+    #                 print("        ts.duration:", ts.duration)
+    #     except (HTTPError, URLError, TimeoutError, InvalidURL) as e:
+    #         print("\033[31mFailed!:\033[0m", e)
+            
+
+        # print("-" * 70)
 
 # with open('tr.m3u', 'r', encoding='utf8') as f:
 #     lines = f.readlines()
