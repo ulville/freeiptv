39400bf090801073471953ef8e115595194115a0
ulville <50755946+ulville@users.noreply.github.com>
2023-12-24 01:06:18 +0300

    remove github workflows before committing and exit on error

--- run-pfk.sh
+++ run-pfk.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
 
 echo "Fetching Origin"
-git fetch &&
+git fetch || exit 1
 echo "Pulling from Origin"
-git pull &&
+git pull || exit 1
 echo "Fetching Upstream..."
-git fetch upstream &&
+git fetch upstream || exit 1
 echo "Merging Upstream..."
-git merge upstream/master --no-edit &&
+git merge upstream/master --no-edit || exit 1
 echo "Commiting merged changes..."
-git commit -m "Merge changes from upstream $(date -I)" &&
+git commit -m "Merge changes from upstream $(date -I)" || exit 1
 
 echo "Parsing for Kodi..."
-"$(which python3)" parse-for-kodi.py &&
+"$(which python3)" parse-for-kodi.py || exit 1
 
 echo "Taking backup of unedited tr.m3u..."
 cp tr.m3u tr-unedited.m3u
 
 echo "Replacing with liveproxy links"
-"$(which python3)" add_liveproxy_links.py &&
+"$(which python3)" add_liveproxy_links.py || exit 1
 
 echo "Removing failing channels..."
-"$(which python3)" testlinks.py --override &&
+"$(which python3)" testlinks.py --override || exit 1
+
+echo "Removing github workflows..."
+rm -r .github/workflows
 
 echo "Adding parsing results..."
-git add . &&
+git add . || exit 1
 echo "Commiting parsing results..."
-git commit -m "parsed for kodi $(date -I)" &&
+git commit -m "parsed for kodi $(date -I)" || exit 1
 echo "Pushing to remote..."
 git push
