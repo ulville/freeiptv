44027c17955940032c050d46d7d3436139f47cc9
ulville <50755946+ulville@users.noreply.github.com>
2024-02-08 02:10:03 +0300

    yellow messages

--- run-pfk.sh
+++ run-pfk.sh
@@ -1,34 +1,40 @@
 #!/bin/bash
 
-echo "Fetching Origin"
+cecho () {
+    YLW='\033[0;33m'
+    RST='\033[0m'
+    echo -e "${YLW}$1${RST}"
+}
+
+cecho "Fetching Origin"
 git fetch || exit 1
-echo "Pulling from Origin"
+cecho "Pulling from Origin"
 git pull || exit 1
-echo "Fetching Upstream..."
+cecho "Fetching Upstream..."
 git fetch upstream || exit 1
-echo "Merging Upstream..."
+cecho "Merging Upstream..."
 git merge upstream/master --no-edit || exit 1
-echo "Commiting merged changes..."
+cecho "Commiting merged changes..."
 git commit -m "Merge changes from upstream $(date -I)"
 
-echo "Parsing for Kodi..."
+cecho "Parsing for Kodi..."
 "$(which python3)" parse-for-kodi.py || exit 1
 
-echo "Taking backup of unedited tr.m3u..."
+cecho "Taking backup of unedited tr.m3u..."
 cp tr.m3u tr-unedited.m3u
 
-echo "Replacing with liveproxy links"
+cecho "Replacing with liveproxy links"
 "$(which python3)" add_liveproxy_links.py || exit 1
 
-echo "Removing failing channels..."
+cecho "Removing failing channels..."
 "$(which python3)" testlinks.py --override || exit 1
 
-echo "Removing github workflows..."
+cecho "Removing github workflows..."
 rm -r .github/workflows
 
-echo "Adding parsing results..."
+cecho "Adding parsing results..."
 git add . || exit 1
-echo "Commiting parsing results..."
+cecho "Commiting parsing results..."
 git commit -m "parsed for kodi $(date -I)" || exit 1
-echo "Pushing to remote..."
+cecho "Pushing to remote..."
 git push
