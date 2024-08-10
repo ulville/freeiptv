8191ab3ad806c41884d6748ad44d14e061e637a9
ulville <ulvilledev@gmail.com>
2024-08-10 23:40:37 +0300

    liveproxy link for KANAL D

--- add_liveproxy_links.py
+++ add_liveproxy_links.py
@@ -2,7 +2,8 @@
 import base64
 
 source_stream_urls = {
-    'ATV': 'https://www.atv.com.tr/canli-yayin'}
+    'ATV': 'https://www.atv.com.tr/canli-yayin',
+    'KANAL D': 'https://www.kanald.com.tr/canli-yayin'}
 
 liveproxy_urls = {name: 'http://localhost:53422/base64/' + base64.b64encode(
     f'streamlink {url} best'.encode(
@@ -12,17 +13,19 @@ liveproxy_urls = {name: 'http://localhost:53422/base64/' + base64.b64encode(
 with open('tr.m3u', 'r', encoding='utf8') as f:
     lines = f.readlines()
 
-for index, line in enumerate(lines):
-    if '",ATV\n' in line:
-        print(lines[index + 1].strip(), 'replaced with')
-        lines[index + 1] = liveproxy_urls['ATV'] + '\n'
-        print(lines[index + 1].strip(), end='\n\n')
-        break
+for name, url in liveproxy_urls.items():
+    for index, line in enumerate(lines):
+        if f'",{name}\n' in line:
+            print(lines[index + 1].strip(), 'replaced with')
+            lines[index + 1] = url + '\n'
+            print(lines[index + 1].strip(), end='\n\n')
+            break
 
-for index, line in enumerate(lines):
-    if '",ATV\n' in line:
-        print(line.strip())
-        print(lines[index + 1].strip())
+for name, url in liveproxy_urls.items():
+    for index, line in enumerate(lines):
+        if f'",{name}\n' in line:
+            print(line.strip())
+            print(lines[index + 1].strip())
 
 with open('tr.m3u', 'w', encoding='utf8') as f:
     f.writelines(lines)
